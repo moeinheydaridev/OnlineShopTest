80c4733609312c934b0f5294b964f74198918047
moeinheydaridev <moeinheydaridev@gmail.com>
2024-09-08 07:48:47 +0100

    Add files via upload

---
+++ OnlineShopTest/DataBase/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShopTest.Models;
+
+
+namespace OnlineShopTest.DataBase
+{
+    public class DBContext : DbContext
+    {
+        public virtual DbSet<CustomerModel> Customers { get; set; }
+
+        public virtual DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=DESKTOP-0TBMFCS;initial catalog=ShopTest;trusted_connection=true;TrustServerCertificate=True");
+        }
+
+    }
+
+}
